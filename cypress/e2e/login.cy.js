b82c1acfd1fd9903c21f7cfb033c301042512423
richardDPR <richard.ingsistemas@gmail.com>
2026-06-27 22:19:05 -0400

    ci: add QA CI Pipeline workflow and Cypress setup

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,10 @@
+describe('Login Test', () => {
+  it('should login successfully', () => {
+    cy.visit('https://www.demoblaze.com');
+    cy.get('#login2').click();
+    cy.get('#loginusername').type('testuser');
+    cy.get('#loginpassword').type('password');
+    cy.get('button[onclick="logIn()"]').click();
+    cy.screenshot('login_test'); // genera login_test.png
+  });
+});
